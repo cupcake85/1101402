76515276c4d9218ee33dd5674f55fa3dd7eee14c
cupcake85 <wanphen9211@gmail.com>
2024-09-10 16:42:42 +0700

    little login

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
 import { Text, View } from "react-native";
+import LogIn from "./login";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>hello</Text>
+    <View style={{ flex: 1 }}>
+      <LogIn />
     </View>
   );
 }
