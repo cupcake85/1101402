c7c79e7f85dc20d952841dbcd026490e1c3497ae
cupcake85 <wanphen9211@gmail.com>
2024-09-13 19:16:30 +0700

    profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -6,7 +6,9 @@ import {
   TouchableOpacity,
   Image,
   Button,
+  ScrollView,
 } from "react-native";
+
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
@@ -14,49 +16,89 @@ import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 export default function ProfileScreen({ navigation }: { navigation: any }) {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flex: 7,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <View style={styles.profileContainer}>
         <Image
           source={require("../assets/images/profile.png")}
-          style={styles.profile}
+          style={styles.profileImage}
         />
-        <View style={{ justifyContent: "center", alignItems: "center" }}>
-          <Text style={{ fontWeight: "bold", fontSize: 30 }}>Digitech SUT</Text>
-          <Text>dgt@digitect.sut.ac.th</Text>
-        </View>
+        <Text style={styles.userName}>DiGiTECH</Text>
+        <Text style={styles.userEmail}>dgt@gmail.com</Text>
       </View>
-      <View style={{ flex: 3, margin: 40 }}>
-        <View style={styles.setlist}>
-          <AntDesign
-            name="setting"
-            size={20}
-            color="black"
-            style={{ marginRight: 2 }}
-          />
-          <Text>Setting</Text>
-        </View>
-        <View style={styles.setlist}>
-          <Ionicons name="help-buoy-outline" size={20} color="black" />
-          <Text>Help</Text>
-        </View>
-        <TouchableOpacity
-          style={styles.setlist}
-          onPress={() => navigation.navigate("Login")}
-        >
-          <MaterialIcons name="logout" size={20} color="black" />
-          <Text>Logout</Text>
-        </TouchableOpacity>
 
-        <Button
-          title="Go to Login"
-          onPress={() => navigation.navigate("Login")}
-        />
+      {/* ส่วนของ My Account */}
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>My account</Text>
+        <View style={styles.card}>
+          <TouchableOpacity style={styles.menuItem}>
+            <Image
+              source={require("../assets/images/user.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.menuText}>Account</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Image
+              source={require("../assets/images/bookin.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.menuText}>Bookings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Image
+              source={require("../assets/images/cards.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.menuText}>My Cards</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Image
+              source={require("../assets/images/vip.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.menuText}>InSAiider VIP</Text>
+          </TouchableOpacity>
+        </View>
       </View>
+
+      {/* ส่วนของ Settings */}
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Settings</Text>
+        <View style={styles.card}>
+          <TouchableOpacity style={styles.menuItem}>
+            <Image
+              source={require("../assets/images/setting.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.menuText}>Settings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Image
+              source={require("../assets/images/contact.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.menuText}>Help Center</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Image
+              source={require("../assets/images/help.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.menuText}>Contact Us</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* ปุ่ม Log out */}
+      <TouchableOpacity
+        style={styles.logoutButton}
+        onPress={() => navigation.navigate("Login")}
+      >
+        <Text style={styles.logoutText}>Log out</Text>
+        <Image
+          source={require("../assets/images/logout.png")}
+          style={styles.icon}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -64,13 +106,72 @@ export default function ProfileScreen({ navigation }: { navigation: any }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 40,
   },
-  profile: {
+  profileContainer: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  profileImage: {
+    width: 130,
+    height: 130,
     borderRadius: 100,
-    width: 150,
-    height: 150,
+    borderWidth: 10,
+    borderColor: "#8FC9BC",
   },
-  setlist: {
+
+  icon: { width: 25, height: 25 },
+
+  userName: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  userEmail: {
+    fontSize: 16,
+    marginTop: 5,
+  },
+  sectionContainer: {
+    marginTop: 20,
+  },
+  sectionTitle: {
+    fontSize: 16,
+    color: "#B4B4B4",
+    marginBottom: 10,
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    elevation: 5, // สำหรับ Android
+  },
+  menuItem: {
     flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+    paddingLeft: 15,
+    // borderBottomWidth: 1,
+    borderBottomColor: "#f0f0f0",
+  },
+  menuText: {
+    fontSize: 16,
+    marginLeft: 15,
+  },
+  logoutButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  logoutText: {
+    fontSize: 18,
+    color: "#C31919",
+    marginRight: 10,
+    fontWeight: "bold",
   },
 });
